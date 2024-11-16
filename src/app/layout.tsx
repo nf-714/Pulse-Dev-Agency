c3f16926b3889daca2b691111c551a6aaf5c5377
nf-714 <nfuad714@gmail.com>
2024-11-16 21:28:31 +0600

    Final Launch v1 - MetaData - FavIcon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Pulse Studio - Website for Club Branding and Events",
+  title: "Pulse Studio - Building Websites for Club Branding and Events",
   description:
     "Pulse Studio is a dynamic, creative agency specializing in club branding and event management. We design innovative websites that capture the essence of your brand while providing seamless event promotion and management tools. Whether you're looking to create a standout online presence for your nightclub or promote your next big event, Pulse Studio delivers tailor-made solutions to engage your audience and elevate your brand experience.",
   openGraph: {
-    title: "Pulse Studio - Website for Club Branding and Events",
+    title: "Pulse Studio - Building Websites for Club Branding and Events",
     description: "Discover amazing content and experiences on My Awesome App",
     url: "https://pulse-bd.vercel.app/",
-    siteName: "Pulse Studio - Website for Club Branding and Events",
+    siteName: "Pulse Studio - Building Websites for Club Branding and Events",
     images: [
       {
         url: "https://utfs.io/f/PgmqDX4sng1lZ8tIN71mDHkbvR6lEYfF0WiParMy2dujG5x7", // This image should be in your public directory
@@ -36,10 +36,10 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "https://utfs.io/f/PgmqDX4sng1lZ8tIN71mDHkbvR6lEYfF0WiParMy2dujG5x7",
+    icon: "https://utfs.io/f/PgmqDX4sng1lkFjoKfNknWj1YS4Kdf6ZIcoVEu8iFhQl5m3O",
     shortcut:
-      "https://utfs.io/f/PgmqDX4sng1lZ8tIN71mDHkbvR6lEYfF0WiParMy2dujG5x7",
-    apple: "https://utfs.io/f/PgmqDX4sng1lZ8tIN71mDHkbvR6lEYfF0WiParMy2dujG5x7",
+      "https://utfs.io/f/PgmqDX4sng1lkFjoKfNknWj1YS4Kdf6ZIcoVEu8iFhQl5m3O",
+    apple: "https://utfs.io/f/PgmqDX4sng1lkFjoKfNknWj1YS4Kdf6ZIcoVEu8iFhQl5m3O",
   },
   robots: {
     index: true,
